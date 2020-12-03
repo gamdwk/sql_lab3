e4abdbe832fbeae0f109adb810c786baef71cdf4
gamdwk <46856538+gamdwk@users.noreply.github.com>
2020-12-03 18:11:30 +0800

    用户与视图创建，角色授权

---
+++ grant_view.sql
@@ -0,0 +1,3 @@
+--创建视图和角色之后再运行此文件
+GRANT SELECT ON student_check TO student
+GRANT SELECT,UPDATE,INSERT,DELETE ON teacher_check TO teacher
---
+++ view_student_check.sql
@@ -0,0 +1,4 @@
+CREATE VIEW student_check
+as
+SELECT s_id,sname,labid
+From t_student
